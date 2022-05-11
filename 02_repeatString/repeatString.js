688171195bc3e872799e744610abd9e1f6f31566
Mike Hudson <mikehudson106@gmail.com>
2022-05-11 11:12:42 +0100

    Write function to satisfy all tests in test file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
 
+//This function repeats a string x number of times on the same line
+//Returns error if x is less than 1
+//Returns nothing if x is 0
+const repeatString = function(string, num) {
+    if (num == 0) {
+        opString = "";
+    } else if (num < 1) {
+        opString = "ERROR";
+    } else {
+    var opString = string;
+    for (i = 0; i < num - 1; i++) {
+     opString += string;
+    } }
+return opString;
 };
 
 // Do not edit below this line
