1398e1004accb2cc3bc7578795260f91c555a908
Mike Hudson <mikehudson106@gmail.com>
2022-05-12 13:11:59 +0100

    Function outputs array with removed element (arg)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
 const removeFromArray = function() {
 
+//find index of value to remove
+//splice to remove that value by index
+let index = arguments[0].indexOf(arguments[1]);
+arguments[0].splice(index, 1);
+return arguments[0];
 };
-
-// Do not edit below this line
-module.exports = removeFromArray;
+removeFromArray([1, 2, 3, 4], 3)
+module.exports = removeFromArray; 
