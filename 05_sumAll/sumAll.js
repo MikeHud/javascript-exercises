b47ca4cd3e0a8b8f7e31ae6f9ba9e5ddbce8b8a4
Mike Hudson <mikehudson106@gmail.com>
2022-05-12 22:42:15 +0100

    Function adds integers between given limits

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+//count until int2 
+let sum = 0;
+    for (i = int1; i <= int2; i++) {
+        sum += i;
+    }
+return sum; 
 };
-
+//console.log(sumAll(1, 3));
 // Do not edit below this line
 module.exports = sumAll;
