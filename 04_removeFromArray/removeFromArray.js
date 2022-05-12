5659528fff80c346725f2aa5a953bb614634f4b5
Mike Hudson <mikehudson106@gmail.com>
2022-05-12 14:03:34 +0100

    Handles non present values in the array argument by checking index for !== -1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,12 +2,14 @@ const removeFromArray = function() {
 
 //loop through multiple time to delete multiple elements
 for (let i = 1; i < arguments.length; i++) {
-
+    console.log(arguments[i]);
     //find index of value to remove
     let index = arguments[0].indexOf(arguments[i]);
-    //splice to remove that value by index
-    arguments[0].splice(index, 1);
+    //splice to remove that value by index but only if it's valid
+        if (index !== -1) {
+        arguments[0].splice(index, 1);
+        }
     };
-    return arguments[0];
+    return arguments[0];    
 }
 module.exports = removeFromArray; 
