dfcab593ba34fb3f121d365f7ce328724f28144a
Mike Hudson <mikehudson106@gmail.com>
2022-05-12 13:39:16 +0100

    Use For loop to iterate through multiple removals according to function argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,13 @@
 const removeFromArray = function() {
 
-//find index of value to remove
-//splice to remove that value by index
-let index = arguments[0].indexOf(arguments[1]);
-arguments[0].splice(index, 1);
-return arguments[0];
-};
-removeFromArray([1, 2, 3, 4], 3)
+//loop through multiple time to delete multiple elements
+for (let i = 1; i < arguments.length; i++) {
+
+    //find index of value to remove
+    let index = arguments[0].indexOf(arguments[i]);
+    //splice to remove that value by index
+    arguments[0].splice(index, 1);
+    };
+    return arguments[0];
+}
 module.exports = removeFromArray; 
