25175084505a39ae8863f4c05a71573cba7c215b
Mike Hudson <mikehudson106@gmail.com>
2022-05-13 08:18:39 +0100

    Add error handling for negative numbers. Implement count down for when int1 is greater than int2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,23 @@
 const sumAll = function(int1, int2) {
-//count until int2 
+//sum of all integers between given range int1, int2
+//check inputs are not negative
+//find the highest value
+//iterate through until it hits the limit
+//return sum
 let sum = 0;
+
+if (int1 < 0 || int2 < 0) {
+    return "ERROR";
+} else if (int1 < int2) {
     for (i = int1; i <= int2; i++) {
         sum += i;
+    }  
+    return sum; 
+} else if (int1 > int2) {
+    for (i = int1; i >= int2; i--) {
+        sum += i;
     }
-return sum; 
-};
-//console.log(sumAll(1, 3));
-// Do not edit below this line
+    return sum; 
+}
+}
 module.exports = sumAll;
